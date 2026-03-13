92d9c57655baef14573a4871721b046dbe5e8734
Tushar <tusharvashisth4@gmail.com>
2026-03-14 01:58:29 +0530

    added the microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/common/theme-provider";
 import "./globals.css";
+import MicrosoftClarity from "@/components/common/microsoft-clarity";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
+        <MicrosoftClarity />
       </body>
     </html>
   );
